ab77ccd50826404bde8937148e75f72db2f7d34c
Nambokle <135355990+Nambokle@users.noreply.github.com>
2024-05-04 02:15:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,1 @@
-token = '' #โทเคนบอท
-webhook = 'https://discord.com/api/webhooks/1236008045994049648/6uzVc9wRTK-J3PZIy5IGtkS-TV_-j3dYUkG2KfgL8CBL5kQkRS6VONHuG82n2mAnEnCw' #ลิ้งค์เว็ปฮุคแจ้งเตือนเวลามีคนมาซื้อของ
-topup_log = 'https://discord.com/api/webhooks/1236008045994049648/6uzVc9wRTK-J3PZIy5IGtkS-TV_-j3dYUkG2KfgL8CBL5kQkRS6VONHuG82n2mAnEnCw' #ลิ้งค์เว็ปฮุคแจ้งเตือนเติมเงิน
-phone = '' #เบอร์วอเลทไว้รับเงิน
-
-guild_id = 1235634559748411392 #ไอดีเซิฟเวอร์
-channel_id = 1236008220611313735 #ไอดีช่องส่งงาน
+sadsadas
